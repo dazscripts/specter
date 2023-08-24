1e408de38d845597de6df8d7c14c7aa1d430c227
daz <71845855+dazscripts@users.noreply.github.com>
2023-08-24 13:16:04 +0000

    added player mods

--- InGame/Main.lua
+++ InGame/Main.lua
@@ -13,7 +13,7 @@ Window = Fluent:CreateWindow({
 })
 --Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
 Tabs = {
-    Main = Window:AddTab({ Title = "Main", Icon = "" }),
+    Player = Window:AddTab({ Title = "Player", Icon = "user" }),
     Evidence = Window:AddTab({ Title = "Evidence", Icon = "book" }),
     Visuals = Window:AddTab({ Title = "Visuals", Icon = "pencil" }),
     Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
@@ -22,14 +22,6 @@ Tabs = {
 Options = Fluent.Options
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dazscripts/specter/GUI/InGame/Evidence.lua"))()
-
-
-Fluent:Notify({
-    Title = "NOTICE",
-    Content = "Script has loaded",
-    SubContent = "Thank you for using Specter GUI", -- Optional
-    Duration = 5 -- Set to nil to make the notification not disappear
-})
 
 Tabs.Visuals:AddSection("World")
 
@@ -80,7 +72,6 @@ InterfaceManager:SetLibrary(Fluent)
 
 -- Ignore keys that are used by ThemeManager.
 -- (we dont want configs to save themes, do we?)
-SaveManager:IgnoreThemeSettings()
 
 -- You can add indexes of elements the save manager should ignore
 SaveManager:SetIgnoreIndexes({})
@@ -98,9 +89,10 @@ SaveManager:BuildConfigSection(Tabs.Settings)
 Window:SelectTab(1)
 
 Fluent:Notify({
-    Title = "Fluent",
-    Content = "The script has been loaded.",
-    Duration = 8
+    Title = "NOTICE",
+    Content = "Script has loaded",
+    SubContent = "Thank you for using Specter GUI", -- Optional
+    Duration = 5 -- Set to nil to make the notification not disappear
 })
 
 -- You can use the SaveManager:LoadAutoloadConfig() to load a config
---
+++ InGame/Player.lua
@@ -0,0 +1,74 @@
+Tabs.Player:AddSection("Player Customizations")
+
+local third = Tabs.Player:AddToggle("third", {Title = "Enable Third Person", Default = false })
+third:OnChanged(function()
+    if Options.third.Value then
+        plr.MaxCameraZoonDistance = 10000
+        plr.CameraMode = 'Classic'
+    else
+        plr.CameraMode = 'LockFirstPerson'
+    end
+end)
+
+Tabs.Player:AddSlider("FOV", {
+    Title = "Field of View",
+    Description = "Change how much you can see",
+    Default = 70,
+    Min = 0,
+    Max = 120,
+    Rounding = 1,
+    Callback = function(Value)
+        workspace.CurrentCamera.FieldOfView = Value
+    end
+})
+
+Tabs.Player:AddSlider("Gravity", {
+    Title = "Gravity",
+    Description = "Change workspace Gravity",
+    Default = 196.1999969482422,
+    Min = 0,
+    Max = 300,
+    Rounding = 1,
+    Callback = function(Value)
+        workspace.Gravity = Value
+    end
+})
+
+
+Tabs.Player:AddSection("Character Mods")
+
+Tabs.Player:AddSlider("WS", {
+    Title = "Walkspeed",
+    Description = "Change how fast your Avatar is",
+    Default = 6,
+    Min = 0,
+    Max = 70,
+    Rounding = 1,
+    Callback = function(Value)
+        plr.Character.Humanoid.Walkspeed = Value
+    end
+})
+
+Tabs.Player:AddSlider("JP", {
+    Title = "JumpPower",
+    Description = "Change how high You can jump.",
+    Default = 0,
+    Min = 0,
+    Max = 120,
+    Rounding = 1,
+    Callback = function(Value)
+        plr.Character.Humanoid.JumpPower = Value
+    end
+})
+
+Tabs.Player:AddSlider("Hip", {
+    Title = "Hip Height",
+    Description = "Change how tall your Avatar is",
+    Default = 2.1924233436584473,
+    Min = 0,
+    Max = 7,
+    Rounding = 1,
+    Callback = function(Value)
+        plr.Character.Humanoid.HipHeight = Value
+    end
+})
