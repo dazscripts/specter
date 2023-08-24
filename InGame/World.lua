6efa1f5fc84b8b36e666318945a88f9adfc1882c
daz <71845855+dazscripts@users.noreply.github.com>
2023-08-24 15:41:46 +0000

    g

--- InGame/World.lua
+++ InGame/World.lua
@@ -1,5 +1,5 @@
 Tabs.World:AddSection("FE Scripts")
-local forcelights = Tabs.Player:AddToggle("forcelights", {Title = "Spam Lights", Default = false })
+local forcelights = Tabs.World:AddToggle("forcelights", {Title = "Spam Lights", Default = false })
 local event = events.Lightswitch
 
 forcelights:OnChanged(function()
@@ -13,12 +13,12 @@ forcelights:OnChanged(function()
         task.wait(0.1)
     until Options.forcelights.Value == false
 end)
-local spamchat = Tabs.Player:AddToggle("spamchat", {Title = "Spam Chat", Default = false })
+local spamchat = Tabs.World:AddToggle("spamchat", {Title = "Spam Chat", Default = false })
 spamchat:OnChanged(function()
     if Options.spamchat.Value == false then return end
 end)
 
-local breakghost = Tabs.Player:AddToggle("breakghost", {Title = "Break Ghost Hunting (Godmode)", Default = false })
+local breakghost = Tabs.World:AddToggle("breakghost", {Title = "Break Ghost Hunting (Godmode)", Default = false })
 breakghost:OnChanged(function()
 
 end)
