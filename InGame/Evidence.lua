cc3dbde0fa7aa33a113e971f0f9a4e5c0cf00711
daz <71845855+dazscripts@users.noreply.github.com>
2023-08-24 11:44:01 +0000

    added fingerprints

--- InGame/Evidence.lua
+++ InGame/Evidence.lua
@@ -1,5 +1,14 @@
 Tabs.Evidence:AddSection("Detect Evidence")
 
+local function PutEvidence(evidence)
+    local gui = plr.PlayerGui.Gui.Journal.Content.Evidence.EvidenceCheckboxes[evidence].Box
+    if if not gui:FindFirstChild("Selected").Visible then
+        for i,v in pairs(getconnections(gui.Activated)) do
+            v:Fire()
+        end
+    end
+end
+
 local orbs = Tabs.Evidence:AddToggle("Orbs", {Title = "Auto Detect Ghost Orbs", Default = false })
 
 local fp = Tabs.Evidence:AddToggle("Fingerprints", {Title = "Auto Detect Fingerprints", Default = false })
@@ -19,11 +28,7 @@ workspace.Orbs.ChildAdded:Connect(function()
                 SubContent = "Evidence is being entered in the Journal.", -- Optional
                 Duration = 5 -- Set to nil to make the notification not disappear
             })
-            if game:GetService("Players").LocalPlayer.PlayerGui.Gui.Journal.Content.Evidence.EvidenceCheckboxes.Orbs.Box:FindFirstChild("Selected").Visible == false then
-                for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Gui.Journal.Content.Evidence.EvidenceCheckboxes.Orbs.Box.Activated)) do
-                    v:Fire()
-                end
-            end
+            PutEvidence("Orbs")
         else
             Fluent:Notify({
                 Title = "Evidence",
@@ -35,7 +40,25 @@ workspace.Orbs.ChildAdded:Connect(function()
     end
 end)
 
-
+workspace.Fingerprints.ChildAdded:Connect(function()
+    if Options.Fingerprints.Value then
+        if Options.auto.Value then
+            Fluent:Notify({
+                Title = "Evidence",
+                Content = "Fingerprints have been detected!",
+                SubContent = "Evidence is being entered in the Journal.", -- Optional
+                Duration = 5 -- Set to nil to make the notification not disappear
+            })
+            PutEvidence("Fingerprints")
+        else
+            Fluent:Notify({
+                Title = "Evidence",
+                Content = "Fingerprints have been detected!",
+                Duration = 5 -- Set to nil to make the notification not disappear
+            })
+        end
+    end
+end)
 
 Tabs.Evidence:AddSection("Objectives")
 local a = workspace.Van.Objectives.SurfaceGui.Frame.Objectives
