5c013c71c4c5194df795d6e33c3b5c94aba06922
daz <71845855+dazscripts@users.noreply.github.com>
2023-08-26 19:39:36 +0000

    fix

--- InGame/MainPage.lua
+++ InGame/MainPage.lua
@@ -52,5 +52,5 @@ local Pickup = Tabs.World:AddDropdown("Grab", {
 })
 local module = require(plr.PlayerScripts.ClientMain.Modules.PromptManager)
 Pickup:OnChanged(function()
-    module.HandlePrompt(workspace.Equipment[Options.Grab.Value].Main.PickupPrompt,plr)
+    module.HandlePrompt(workspace.Equipment:FindFirstChild(Options.Grab.Value).Main.PickupPrompt, plr)
 end)
